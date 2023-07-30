def2b72143578c1205335b5a74ea63894f2be508
dopswae98 <ephraimmatarutse@gmail.com>
2023-07-31 01:28:03 +0200

    added font awesome libraries

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
+// import { far } from "@fortawesome/free-regular-svg-icons";
 
 import { Routes, Route } from "react-router-dom";
 import CompanyList from "./Pages/CompanyList";
