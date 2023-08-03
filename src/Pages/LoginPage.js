cb5241a41a8380ae15ef4d9e4b34dc856e29cc09
dopswae98 <ephraimmatarutse@gmail.com>
2023-08-03 14:25:04 +0200

    resolved navigation issue

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import NavbarComponent from "../Components/NavbarComponent";
 import axios from "axios";
 import { Link, Navigate } from "react-router-dom";
